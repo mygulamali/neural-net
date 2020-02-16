7ae2f901cc8146492f97dcf868bc93f2b0c1b30d
Murtaza Gulamali <murtaza@gulamali.net>
2020-02-16 14:10:36 +0000

    Refactor tests for network creation and destruction

--- tests/nn_network_tests.c
+++ tests/nn_network_tests.c
@@ -11,30 +11,24 @@ void test_nn_network_create_destroy(void **state) {
     for (size_t i = 0; i < n; i++)
 	assert_int_equal(network->layers[i], layers[i]);
 
+    gsl_matrix *zeros;
+
     for (size_t i = 0; i < n - 1; i++) {
 	gsl_matrix *bias = network->biases[i];
 	assert_int_equal(bias->size1, layers[i + 1]);
 	assert_int_equal(bias->size2, 1);
 
-	for (size_t ii = 0; ii < bias->size1; ii++)
-	    for (size_t jj = 0; jj < bias->size2; jj++)
-		assert_double_equal(
-		    gsl_matrix_get(bias, ii, jj),
-		    0.0,
-		    EPSILON
-		);
+	zeros = gsl_matrix_calloc(bias->size1, 1);
+	assert_gsl_matrix_equal(bias, zeros, EPSILON);
+	gsl_matrix_free(zeros);
 
 	gsl_matrix *weight = network->weights[i];
 	assert_int_equal(weight->size1, layers[i + 1]);
 	assert_int_equal(weight->size2, layers[i]);
 
-	for (size_t ii = 0; ii < weight->size1; ii++)
-	    for (size_t jj = 0; jj < weight->size2; jj++)
-		assert_double_equal(
-		    gsl_matrix_get(weight, ii, jj),
-		    0.0,
-		    EPSILON
-		);
+	zeros = gsl_matrix_calloc(weight->size1, weight->size2);
+	assert_gsl_matrix_equal(weight, zeros, EPSILON);
+	gsl_matrix_free(zeros);
     }
 
     nn_network_destroy(network);
